0620a491e17dc37c5fda249370146935ada47795
Unknown <mikkelbjerg95@gmail.com>
2021-04-21 11:01:22 +0200

    Auto stash before merge of "main" and "origin/main"

--- orchestrator/src/main/java/com/github/lukelinkwalker/orchestrator/Util/StringUtilities.java
+++ orchestrator/src/main/java/com/github/lukelinkwalker/orchestrator/Util/StringUtilities.java
@@ -4,4 +4,47 @@ public class StringUtilities {
     public static String removeTokensFromString(String string) {
         return string.substring(16, string.length() - 16);
     }
+    
+    public static boolean isInteger(String str) {
+    	for(int i = 0; i < str.length(); i += 1) {
+    		char c = str.charAt(i);
+    		
+    		if(Character.isDigit(c) == false) {
+    			return false;
+    		}
+    	}
+    	
+    	return true;
+    }
+    
+    public static boolean isFloat(String str) {
+    	int commas = 0;
+    	
+    	for(int i = 0; i < str.length(); i += 1) {
+    		char c = str.charAt(i);
+    		
+    		if(c == '.') {
+    			if(commas == 0) {
+    				commas += 1;
+    				continue;
+    			} else {
+    				return false;
+    			}
+    		}
+    		
+    		if(Character.isDigit(c) == false) {
+    			return false;
+    		}
+    	}
+    	
+    	return true;
+    }
+    
+    public static boolean isBoolean(String str) {
+    	if(str.toLowerCase().equals("true") || str.toLowerCase().equals("false")) {
+    		return true;
+    	}
+    	
+    	return false;
+    }
 }
